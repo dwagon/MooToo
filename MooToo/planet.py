38d045f61a2a52aa8ffbe95cd5061ef6c118fb24
Dougal Scott <dougal.scott@gmail.com>
2024-04-23 12:32:06 +1000

    Planet climate, richness and gravity

--- MooToo/planet.py
+++ MooToo/planet.py
@@ -1,7 +1,9 @@
 """ Planet class """
 
 import random
-from enum import Enum, auto
+from typing import Any
+from enum import Enum, StrEnum, auto
+from MooToo.utils import prob_map
 
 
 class PlanetCategory(Enum):
@@ -10,23 +12,80 @@ class PlanetCategory(Enum):
     GAS_GIANT = auto()
 
 
-class PlanetSize(Enum):
-    TINY = auto()
-    SMALL = auto()
-    MEDIUM = auto()
-    LARGE = auto()
-    HUGE = auto()
+class PlanetSize(StrEnum):
+    TINY = "T"
+    SMALL = "S"
+    MEDIUM = "M"
+    LARGE = "L"
+    HUGE = "H"
+
+
+class PlanetGravity(StrEnum):
+    LOW = "L"
+    NORMAL = "N"
+    HIGH = "H"
+
+
+class PlanetRichness(Enum):
+    ULTRA_POOR = "UP"
+    POOR = "P"
+    ABUNDANT = "A"
+    RICH = "R"
+    ULTRA_RICH = "UR"
+
+
+class PlanetClimate(StrEnum):
+    TOXIC = "TX"
+    RADIATED = "R"
+    BARREN = "B"
+    DESERT = "D"
+    TUNDRA = "TU"
+    OCEAN = "O"
+    SWAMP = "S"
+    ARID = "A"
+    TERRAN = "TE"
+    GAIA = "G"
 
 
 class Planet:
-    def __init__(self, name, star_colour):
+    def __init__(self, name: str, config: dict[str, Any]):
         self.name = name
         self.category = pick_planet_category()
         self.size = pick_planet_size()
+        self.richness = pick_planet_richness(config["richness"])
+        self.climate = pick_planet_climate(config["climate"])
+        self.gravity = pick_planet_gravity(self.size, self.richness)
         self.arc = random.randint(0, 359)
 
+    def __repr__(self):
+        return f"<Planet {self.name}: {self.category.name} {self.size.name} {self.richness.name} {self.climate.name} {self.gravity.name}>"
 
-def pick_planet_size():
+
+def pick_planet_climate(config: dict[str, int]) -> PlanetClimate:
+    """Climate of the planet depends on the star colour"""
+    climate = prob_map(config)
+    return PlanetClimate(climate)
+
+
+def pick_planet_richness(config: dict[str, int]) -> PlanetRichness:
+    richness = prob_map(config)
+    return PlanetRichness(richness)
+
+
+def pick_planet_gravity(size: PlanetSize, richness: PlanetRichness) -> PlanetGravity:
+    """The bigger and richer the planet the higher the gravity"""
+    grav_map = {
+        "T": {"UP": "L", "P": "L", "A": "L", "R": "N", "UR": "N"},
+        "S": {"UP": "L", "P": "L", "A": "N", "R": "N", "UR": "N"},
+        "M": {"UP": "L", "P": "N", "A": "N", "R": "N", "UR": "H"},
+        "L": {"UP": "N", "P": "N", "A": "N", "R": "H", "UR": "H"},
+        "H": {"UP": "N", "P": "N", "A": "H", "R": "H", "UR": "H"},
+    }
+    gravity = grav_map[size.value][richness.value]
+    return PlanetGravity(gravity)
+
+
+def pick_planet_size() -> PlanetSize:
     pct = random.randrange(1, 100)
     if pct < 10:
         return PlanetSize.TINY
@@ -39,7 +98,7 @@ def pick_planet_size():
     return PlanetSize.HUGE
 
 
-def pick_planet_category():
+def pick_planet_category() -> PlanetCategory:
     """What sort of planet is this?"""
     pct = random.randrange(1, 100)
     if pct < 20:
---
+++ MooToo/utils.py
@@ -0,0 +1,18 @@
+import random
+
+
+#################################################################################################
+def prob_map(d):
+    """Given a dictionary of choice probabilities {'a': 10, 'b': 20, ...}
+    return a random choice based on the probability"""
+    totprob = sum(d.values())
+    r = random.randrange(totprob)
+    for k, v in d.items():
+        if r < v:
+            return k
+        r -= v
+    else:
+        return None
+
+
+# EOF
