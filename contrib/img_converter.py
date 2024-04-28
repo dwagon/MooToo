0479e7812e4bc51d5d1e909c877617a6956f5b39
Dougal Scott <dougal.scott@gmail.com>
2024-04-28 11:18:37 +1000

    Sort of progress

--- contrib/img_converter.py
+++ contrib/img_converter.py
@@ -2,11 +2,19 @@
 """ Convert Moo2 files to useful images
     Useful info taken from http://www.karoltomala.com/blog/?p=39
 """
-
+import os.path
 import struct
 import sys
+from enum import Enum, auto
 from typing import Optional, Any
 from dataclasses import dataclass, field
+from PIL import Image, ImageDraw
+
+
+class LineType(Enum):
+    NORMAL = auto()
+    NEW_LINE = auto()
+    END_DATA = auto()
 
 
 ##############################################################################
@@ -15,9 +23,9 @@ from dataclasses import dataclass, field
 class Line:
     """Handle a line of pixels, possibly indented"""
 
-    pixels: bytearray = field(default_factory=bytearray)
+    pixels: list[int] = field(default_factory=list)
     indent: int = 0
-    special: int = 0
+    special: LineType = LineType.NORMAL
 
 
 ##############################################################################
@@ -32,45 +40,110 @@ class Frame:
 
 
 ##############################################################################
-def image_flags(flag_seq: int) -> dict[str, bool]:
-    """Pull out image flags"""
-    return {
-        "uncompressed": bool(flag_seq & 0x0100),
-        "background": bool(flag_seq & 0x0400),
-        "functional": bool(flag_seq & 0x0800),
-        "internal": bool(flag_seq & 0x1000),
-        "junction": bool(flag_seq & 0x2000),
-    }
+def load_palette(fname: str) -> dict[int, tuple[int, int, int]]:
+    palette: dict[int, tuple[int, int, int]] = {}
+    with open(fname, "rb") as infd:
+        for counter in range(256):
+            _ = struct.unpack("B", infd.read(1))[0]
+            red = struct.unpack("B", infd.read(1))[0]
+            green = struct.unpack("B", infd.read(1))[0]
+            blue = struct.unpack("B", infd.read(1))[0]
+            palette[counter] = (red * 4, green * 4, blue * 4)
+    return palette
 
 
 ##############################################################################
-def process_frame(fd) -> Optional[Frame]:
-    """Process the frame data"""
-    frame_indicator = dread(fd)
+##############################################################################
+@dataclass
+class Graphic:
+    flags: dict[str, bool] = field(default_factory=dict)
+    width: int = 0
+    height: int = 0
+    num_frames: int = 0
+    delay: int = 0
+    frames: list[Frame] = field(default_factory=list)
 
-    if frame_indicator != 1:
-        return None
-    frame_y_indent = dread(fd)
-    frame = Frame(indent=frame_y_indent)
-    while True:
-        pixels = dread(fd)
-        if pixels == 0:
-            y_indent = dread(fd)
-            if y_indent == 1000:  # End of Data
-                line = Line(special=2, indent=y_indent)
+    ##############################################################################
+    def load(self, fd) -> None:
+        """Process the filename"""
+        self.width = dread(fd)
+        self.height = dread(fd)
+        _ = fd.read(2)  # Nothing in the next two chars
+
+        self.num_frames = dread(fd)
+        self.delay = dread(fd)
+        self.flags = self.image_flags(dread(fd))
+        # print(f"{self.width} x {self.height} frames={self.num_frames} {self.flags}")
+        frame_offsets = []
+        for _ in range(self.num_frames + 1):
+            offset = dread(fd, "<L", 4)
+            frame_offsets.append(offset)
+        for offset in frame_offsets[:-1]:
+            fd.seek(offset)
+            self.frames.append(self.process_frame(fd))
+
+    ##############################################################################
+    def image_flags(self, flag_seq: int) -> dict[str, bool]:
+        """Pull out image flags"""
+        return {
+            "compressed": bool(flag_seq & 0x0100),
+            "background": bool(flag_seq & 0x0400),
+            "functional": bool(flag_seq & 0x0800),
+            "internal": bool(flag_seq & 0x1000),
+            "junction": bool(flag_seq & 0x2000),
+        }
+
+    ##############################################################################
+    def process_frame(self, fd) -> Optional[Frame]:
+        """Process the frame data"""
+        frame_indicator = dread(fd)
+
+        if frame_indicator != 1:
+            return None
+        frame_y_indent = dread(fd)
+        frame = Frame(indent=frame_y_indent)
+        while True:
+            pixels = dread(fd)
+            if pixels == 0:
+                y_indent = dread(fd)
+                if y_indent == 1000:  # End of Data
+                    line = Line(special=LineType.END_DATA, indent=y_indent)
+                    frame.lines.append(line)
+                    break
+                line = Line(special=LineType.NEW_LINE, indent=y_indent)
                 frame.lines.append(line)
-                break
-            line = Line(special=1, indent=y_indent)
+            else:
+                x_indent = dread(fd)
+                byte_fmt = "<" + "B" * pixels
+                colour_array = struct.unpack(byte_fmt, fd.read(pixels))
+                line = Line(indent=x_indent, pixels=colour_array)
+                if pixels % 2:
+                    fd.read(1)
             frame.lines.append(line)
-        else:
-            x_indent = dread(fd)
-            byte_fmt = "<" + "B" * pixels
-            colour_array = dread(fd, byte_fmt, pixels)
-            line = Line(indent=x_indent, pixels=colour_array)
-            if pixels % 2:
-                fd.read(1)
-        frame.lines.append(line)
-    return frame
+        return frame
+
+    ##############################################################################
+    def save_frame(self, filename: str, palette: dict[int, tuple[int, int, int]], frame: int = 0) -> None:
+        """Same frame as an image"""
+        f = self.frames[frame]
+        image = Image.new("P", (self.width, self.height), "white")
+        draw = ImageDraw.Draw(image)
+        rel_x = 0
+        rel_y = f.indent
+
+        for line in f.lines:
+            if line.special == LineType.NORMAL:
+                rel_x += line.indent
+                for x in range(len(line.pixels)):
+                    colour = palette[line.pixels[x]]
+                    draw.point((rel_x + x, rel_y), fill=colour)
+                    print(f"DBG {rel_x+x}, {rel_y}")
+            else:
+                rel_y += line.indent
+                print(f"DBG {rel_y=} {line.indent=}")
+                rel_x = 0
+        with open(filename, "wb") as outfh:
+            image.save(outfh, "PNG")
 
 
 ##############################################################################
@@ -79,34 +152,21 @@ def dread(fd, format: str = "<H", bytes: int = 2) -> Any:
 
 
 ##############################################################################
-def process(fd) -> None:
-    """Process the filename"""
-    width = dread(fd)
-    height = dread(fd)
-    _ = fd.read(2)  # Nothing in the next two chars
-
-    frames = dread(fd)
-    frame_delay = dread(fd)
-    flags = image_flags(dread(fd))
-    print(f"{width} x {height} @ {frames} frames {frame_delay} delay")
-    print(f"Flags {flags}")
-    frame_offsets = []
-    for _ in range(frames + 1):
-        offset = dread(fd, "<L", 4)
-        frame_offsets.append(offset)
-    print(f"Offsets = {frame_offsets}")
-    for offset in frame_offsets[:-1]:
-        fd.seek(offset)
-        x = process_frame(fd)
-        print(x)
-
-
-##############################################################################
 def main() -> None:
     """Do the stuff"""
-    file = sys.argv[1]
-    with open(file, "rb") as fd:
-        process(fd)
+    palettes = [f"../foo/FONTS.LBX_{_}" for _ in range(1, 14)] + [f"../foo/IFONTS.LBX_{_}" for _ in range(1, 4)]
+    for num, palfile in enumerate(palettes):
+        palette = load_palette(palfile)
+        for file in sys.argv[1:]:
+            try:
+                g = Graphic()
+                with open(file, "rb") as fd:
+                    g.load(fd)
+                save_file = f"{os.path.basename(file)}_{num}.png"
+                g.save_frame(save_file, palette)
+            except Exception as exc:
+                print(f"Failure on {file}: {exc}")
+                raise
 
 
 ##############################################################################
