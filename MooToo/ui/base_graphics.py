c981055d4e43ea936f017c774047d4e44f3693fb
Dougal Scott <dougal.scott@gmail.com>
2024-09-27 09:24:33 +1000

    Dashed line code

--- MooToo/ui/base_graphics.py
+++ MooToo/ui/base_graphics.py
@@ -111,6 +111,51 @@ class BaseGraphics:
 
 
 #####################################################################################################
+# Taken from https://codereview.stackexchange.com/a/70206
+class Point:
+    # constructed using a normal tuple
+    def __init__(self, point_t=(0, 0)):
+        self.x = float(point_t[0])
+        self.y = float(point_t[1])
+
+    # define all useful operators
+    def __add__(self, other: Self) -> Self:
+        return Point((self.x + other.x, self.y + other.y))
+
+    def __sub__(self, other: Self) -> Self:
+        return Point((self.x - other.x, self.y - other.y))
+
+    def __mul__(self, scalar: Any) -> Self:
+        return Point((self.x * scalar, self.y * scalar))
+
+    def __truediv__(self, scalar: Any) -> Self:
+        return Point((self.x / scalar, self.y / scalar))
+
+    def __len__(self) -> int:
+        return int(math.sqrt(self.x**2 + self.y**2))
+
+    # get back values in original tuple format
+    def get(self):
+        return self.x, self.y
+
+
+#####################################################################################################
+def draw_dashed_line(surf, color, start_pos, end_pos, width=1, dash_length=10):
+    origin = Point(start_pos)
+    target = Point(end_pos)
+    displacement = target - origin
+    length = len(displacement)
+    if length == 0:
+        return
+    slope = displacement / length
+
+    for index in range(0, int(length / dash_length), 2):
+        start = origin + (slope * index * dash_length)
+        end = origin + (slope * (index + 1) * dash_length)
+        pygame.draw.line(surf, color, start.get(), end.get(), width)
+
+
+#####################################################################################################
 def load_image(
     lbx_file: str, lbx_index: int, frame: int = 0, palette_file="FONTS.LBX", palette_index=1
 ) -> pygame.Surface:
